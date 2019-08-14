53e82dab7716a06b6f1de7add035ad7276dd9c67
sairim <spiri@protonmail.com>
2019-08-14 15:25:24 +0300

    implement create link mutation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/App.css';
-import LinkList from './LinkList';
+import "../styles/App.css";
+import CreateLink from "./CreateLink";
 
 function App() {
-  return <LinkList />
+  return <CreateLink />;
 }
 
 export default App;
